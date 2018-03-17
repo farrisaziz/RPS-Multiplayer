5b3e87f8be64d21293f83a0541f040c3549cebe7
Farris <farrisaziz18@gmail.com>
2018-03-17 08:19:24 -0400

    Files Added

---
+++ assets/js/config.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+<script src="https://www.gstatic.com/firebasejs/4.11.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCo9XMRu0mkmsUpRzt8u_VyTOyKj6zMnDw",
+    authDomain: "multi-rps-5ab0f.firebaseapp.com",
+    databaseURL: "https://multi-rps-5ab0f.firebaseio.com",
+    projectId: "multi-rps-5ab0f",
+    storageBucket: "multi-rps-5ab0f.appspot.com",
+    messagingSenderId: "620996623945"
+  };
+  firebase.initializeApp(config);
+</script>
